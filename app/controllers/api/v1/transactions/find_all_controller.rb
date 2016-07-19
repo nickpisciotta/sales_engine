a110cc4d059e82ce2a69c48e06b9665bbcbc4f41
Lucy Fox <lucyreneefox@gmail.com>
2016-07-18 19:44:17 -0600

    Add find, find_all and random functionality for merchants

---
+++ app/controllers/api/v1/transactions/find_all_controller.rb
@@ -0,0 +1,12 @@
+class Api::V1::Transactions::FindAllController < ApplicationController
+  respond_to :json, :xml
+
+  def index
+    respond_with Transaction.where(transaction_all_params)
+  end
+
+  private
+    def transaction_all_params
+      params.permit(:id, :credit_card_number, :credit_card_expiration_date, :result, :created_at, :updated_at)
+    end
+end
