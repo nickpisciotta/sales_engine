05ee50951ab9279d4f6e147cb2972b25373488aa
Nick Pisciotta <npisciotta@gmail.com>
2016-07-18 22:19:06 -0600

    Clean up routes file

--- config/routes.rb
+++ config/routes.rb
@@ -23,33 +23,27 @@ Rails.application.routes.draw do
       end
       resources :transactions, only: [:index, :show]
 
-
-    end
-  end
-
-  namespace :api do
-    namespace :v1 do
       namespace :items do
         get '/find', to: "find#show"
         get '/find_all', to: "find_all#index"
         get '/random', to: "random#show"
       end
+      resources :items, only: [:index, :show], defaults: {format: :json}
 
       namespace :invoices do
         get '/find', to: "find#show"
         get '/find_all', to: "find_all#index"
         get '/random', to: "random#show"
       end
+      resources :invoices, only: [:index, :show], defaults: {format: :json}
 
       namespace :invoice_items do
         get '/find', to: "find#show"
         get '/find_all', to: "find_all#index"
         get '/random', to: "random#show"
       end
-
-      resources :items, only: [:index, :show], defaults: {format: :json}
-      resources :invoices, only: [:index, :show], defaults: {format: :json}
       resources :invoice_items, only: [:index, :show], defaults: {format: :json}
+
     end
   end
 end
