d275b7411a38276daf8e3f3b2d3877ce6b154073
Lucy Fox <lucyreneefox@gmail.com>
2016-07-19 20:24:22 -0600

    Transaction controller test within invoice complete

--- spec/requests/api/V1/invoices/invoice_items_controller_spec.rb
+++ spec/requests/api/V1/invoices/invoice_items_controller_spec.rb
@@ -1,1 +1,17 @@
 require 'rails_helper'
+
+describe "Invoice, InvoiceItemsController" do
+  it "returns all invoice items for invoice" do
+    invoice = create(:invoice)
+    invoice_item1, invoice_item2 = create_list(:invoice_item, 2, invoice: invoice, quantity: 4)
+    invoice_item3 = create(:invoice_item, quantity: 3)
+    id = invoice.id
+
+    get "/api/v1/invoices/#{id}/invoice_items"
+
+    quantities = json.map { |invoice_item| invoice_item['quantity'] }
+    expect(response).to be_success
+    expect(json.count).to eq(2)
+    expect(quantities).to_not include(3)
+  end
+end
