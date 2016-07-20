43e9b661f569780e04841e8697819eca109f9622
Lucy Fox <lucyreneefox@gmail.com>
2016-07-20 09:32:32 -0600

    Serializer working for unit price for both invoices and for items

--- app/models/invoice_item.rb
+++ app/models/invoice_item.rb
@@ -3,10 +3,4 @@ class InvoiceItem < ApplicationRecord
   belongs_to :item
   belongs_to :invoice
   default_scope { order('id ASC') }
-
-  def format_unit_price
-    if unit_price.to_i.to_s != unit_price
-      self.unit_price = (self.unit_price.to_f * 100).to_i
-    end
-  end
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -4,7 +4,3 @@ class Item < ApplicationRecord
   has_many :invoices, through: :invoice_items
   default_scope { order('id ASC') }
 end
-
-
-
-#########################TO get spec harness to pass probably have to be storing as a string in db
