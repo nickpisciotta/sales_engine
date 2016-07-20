c57603eba588b2bef99c32d7812d423e3c18b2e0
Lucy Fox <lucyreneefox@gmail.com>
2016-07-19 20:11:25 -0600

    Customer invoice spec passing

--- spec/requests/api/V1/invoices/customer_controller_spec.rb
+++ spec/requests/api/V1/invoices/customer_controller_spec.rb
@@ -1,1 +1,14 @@
 require 'rails_helper'
+
+describe "Invoices, Customer Controller" do
+  it "returns correct customer for an invoice" do
+    customer = create(:customer, first_name: "TestCustomer")
+    invoice = create(:invoice, customer: customer)
+    id = invoice.id
+
+    get "/api/v1/invoices/#{id}/customer"
+
+    expect(response).to be_success
+    expect(json['first_name']).to eq("TestCustomer")
+  end
+end
