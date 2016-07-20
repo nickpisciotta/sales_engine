70568c2394a2eebe626661222a25ff3679a9c37d
Lucy Fox <lucyreneefox@gmail.com>
2016-07-19 19:41:30 -0600

    Items invoice items and merchant specs passing

--- spec/requests/api/V1/items/invoice_items_spec.rb
+++ spec/requests/api/V1/items/invoice_items_spec.rb
@@ -1,1 +1,22 @@
 require 'rails_helper'
+
+describe "Items, Invoice Item Controller" do
+  describe "returns collection of invoice items" do
+    it "returns correct invoice items" do
+      item = create(:item, name: "test_item")
+      item2 = create(:item, name: "other_item")
+
+      invoice_item1, invoice_item2 = create_list(:invoice_item, 2, item: item, quantity: 3)
+      invoice_item3 = create(:invoice_item, item: item2, quantity: 6)
+
+      id = item.id
+
+      get "/api/v1/items/#{id}/invoice_items"
+
+      quantities = json.map {|invoice_item| invoice_item['quantity'] }
+      expect(response).to be_success
+      expect(json.count).to eq(2)
+      expect(quantities).to_not include(6)
+    end
+  end
+end
--- spec/requests/api/V1/items/merchant_controller_spec.rb
+++ spec/requests/api/V1/items/merchant_controller_spec.rb
@@ -1,1 +1,18 @@
 require 'rails_helper'
+
+describe "Items, Merchant Controller" do
+    it "returns correct merchant for item" do
+      merchant1 = create(:merchant, name: "First Merchant")
+      merchant2 = create(:merchant, name: "Second Merchant")
+
+      item = create(:item, merchant: merchant1)
+
+      id = item.id
+
+      get "/api/v1/items/#{id}/merchant"
+
+      expect(response).to be_success
+      expect(json['name']).to eq("First Merchant")
+      expect(json['name']).to_not eq("Second Merchant")
+    end
+end
