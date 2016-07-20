d8b0e6b25d6b05edcdbedc7148ff51493f2821e6
Lucy Fox <lucyreneefox@gmail.com>
2016-07-19 20:09:46 -0600

    Merchants controller within invoices test passing

--- spec/requests/api/V1/invoices/merchant_controller_spec.rb
+++ spec/requests/api/V1/invoices/merchant_controller_spec.rb
@@ -1,1 +1,14 @@
 require 'rails_helper'
+
+describe "Invoices, Merchant Controller" do
+  it "returns correct merchant for an invoice" do
+    merchant = create(:merchant, name: "TestMerchant")
+    invoice = create(:invoice, merchant: merchant)
+    id = invoice.id
+
+    get "/api/v1/invoices/#{id}/merchant"
+
+    expect(response).to be_success
+    expect(json['name']).to eq("TestMerchant")
+  end
+end
