2165e0134cf007588c62e1947aa35fafccf4a472
Nick Pisciotta <npisciotta@gmail.com>
2016-07-20 22:18:14 -0600

    refactor item model methods

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -4,7 +4,10 @@ class Customer < ApplicationRecord
   default_scope { order('id ASC') }
 
   def favorite_merchant
-    favorite_merchant_hash = transactions.where(result: "success").joins(:invoice).group("invoices.merchant_id").count("invoices.merchant_id")
+    favorite_merchant_hash = transactions.where(result: "success")
+                                         .joins(:invoice)
+                                         .group("invoices.merchant_id")
+                                         .count("invoices.merchant_id")
     merchant_id = favorite_merchant_hash.keys.first
     Merchant.find_by(id: merchant_id)
   end
