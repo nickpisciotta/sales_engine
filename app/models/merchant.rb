98e3871ef57f2e3f5a99eb0170e723c978b4cc72
Lucy Fox <lucyreneefox@gmail.com>
2016-07-20 22:59:25 -0600

    took out extra variable assignments

--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -7,21 +7,24 @@ class Merchant < ApplicationRecord
   default_scope { order('id ASC') }
 
   def find_revenue
-     revenue = invoices.joins(:transactions, :invoice_items).where(transactions: {result: "success"}).sum("quantity * unit_price")
+     invoices.joins(:transactions, :invoice_items).where(transactions: {result: "success"}).sum("quantity * unit_price")
   end
 
   def revenue_by_date(date)
-     invoices.where("invoices.created_at = '#{date}'").joins(:transactions, :invoice_items).where("transactions.result = 'success'").sum("invoice_items.quantity * invoice_items.unit_price")
+     invoices.where("invoices.created_at = '#{date}'")
+     .joins(:transactions, :invoice_items)
+     .where("transactions.result = 'success'")
+     .sum("invoice_items.quantity * invoice_items.unit_price")
   end
 
   def self.with_most_items(num)
-  joins(invoices: [:transactions, :invoice_items])
-  .where(transactions: {result: "success"}).group(:id)
-  .order('sum(invoice_items.quantity) DESC').limit(num)
+    joins(invoices: [:transactions, :invoice_items])
+    .where(transactions: {result: "success"}).group(:id)
+    .order('sum(invoice_items.quantity) DESC').limit(num)
   end
 
   def favorite_customer
-    c = customers.joins(:transactions).where(transactions: {result: "success"}).group(:id).order('count(invoices.merchant_id)DESC').first
+    customers.joins(:transactions).where(transactions: {result: "success"}).group(:id).order('count(invoices.merchant_id)DESC').first
   end
 
   def self.revenue(date)
